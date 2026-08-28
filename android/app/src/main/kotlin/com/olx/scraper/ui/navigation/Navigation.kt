72112ba89a1425ce781090a0fc3a22309f46b7b8
danielsala01-dev <daniel.sala01@gmail.com>
2026-08-28 13:01:26 +0200

    Add navigation graph

---
+++ android/app/src/main/kotlin/com/olx/scraper/ui/navigation/Navigation.kt
@@ -0,0 +1,33 @@
+package com.olx.scraper.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.olx.scraper.ui.screens.HomeScreen
+import com.olx.scraper.ui.screens.CategoriesScreen
+import com.olx.scraper.ui.screens.FavoritesScreen
+import com.olx.scraper.ui.screens.SettingsScreen
+
+@Composable
+fun OLXNavGraph() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable("home") {
+            HomeScreen(navController)
+        }
+        composable("categories") {
+            CategoriesScreen(navController)
+        }
+        composable("favorites") {
+            FavoritesScreen(navController)
+        }
+        composable("settings") {
+            SettingsScreen(navController)
+        }
+    }
+}
